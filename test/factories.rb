0ca9b7ca28e33a5d56a30f085f6e064f8105387b
Adam Miribyan <adam.miribyan@gmail.com>
2011-07-16 03:05:38 +0500

    item factory

---
+++ test/factories.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+FactoryGirl.define do
+  factory :item do
+    name '№5 Parfum Bottle'
+    description 'The now and forever fragrance. The ultimate in femininity.'
+    price 8265
+    gender 'f'
+    volume 15
+    launched_at 1921
+  end
+end
